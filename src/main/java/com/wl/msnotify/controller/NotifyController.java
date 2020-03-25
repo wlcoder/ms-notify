caa9c6cb8e9d4e7ddbe994d7e045101ea4d87161
wlong <wanglong@ysstech.com>
2020-03-25 09:54:53 +0800

    添加日志信息

--- src/main/java/com/wl/msnotify/controller/NotifyController.java
+++ src/main/java/com/wl/msnotify/controller/NotifyController.java
@@ -14,16 +14,12 @@ import com.wl.msnotify.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/notify")
