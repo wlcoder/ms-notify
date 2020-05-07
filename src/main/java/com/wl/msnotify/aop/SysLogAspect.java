ea4863693417243e03c2abd32204b6986becf59d
wlong <wanglong@ysstech.com>
2020-05-07 12:38:25 +0800

    添加ip

--- src/main/java/com/wl/msnotify/aop/SysLogAspect.java
+++ src/main/java/com/wl/msnotify/aop/SysLogAspect.java
@@ -3,6 +3,7 @@ package com.wl.msnotify.aop;
 import com.alibaba.fastjson.JSON;
 import com.wl.msnotify.entity.SysLog;
 import com.wl.msnotify.service.SysLogService;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,8 +11,13 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 /**
@@ -19,6 +25,7 @@ import java.util.Date;
  * @Description :
  * @date : 2020/4/29 16:49
  */
+@Slf4j
 @Aspect
 @Component
 public class SysLogAspect {
@@ -58,8 +65,45 @@ public class SysLogAspect {
         sysLog.setCreateDate(new Date());
         sysLog.setUserName("admin");//默认admin
         //获取用户ip地址
-        sysLog.setIp("127.0.0.1");
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String ipAddr = getRemoteHost(request);
+        sysLog.setIp(ipAddr);
         sysLogService.insertLog(sysLog);
+    }
+
+    /**
+     * 获取目标主机的ip
+     *
+     * @param request
+     * @return
+     */
+    private String getRemoteHost(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if ("127.0.0.1".equals(ip) || "0:0:0:0:0:0:0:1".equals(ip)) {
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        return ip;
     }
 
 }
