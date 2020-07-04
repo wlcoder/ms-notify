3ae38556556b71b8c8d93ea051feda63c8f5a28d
wlong <wanglong@ysstech.com>
2020-07-04 19:13:32 +0800

    默认请求转发到登录页面

--- src/main/java/com/wl/msnotify/config/webConfig.java
+++ src/main/java/com/wl/msnotify/config/webConfig.java
@@ -4,6 +4,7 @@ import com.wl.msnotify.interceptor.AuthenticationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -22,4 +23,17 @@ public class webConfig implements WebMvcConfigurer {
     public AuthenticationInterceptor authenticationInterceptor() {
         return new AuthenticationInterceptor();
     }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        WebMvcConfigurer adapter = new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return adapter;
+    }
+
 }
